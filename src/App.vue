93cfd99a866583104dc794ad1c90f504cda5aaba
Joshualeexy <oghenekevwejoshualee@gmail.com>
2024-04-04 13:38:18 +0100

    update word counter function

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,49 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+const string = ref('');
+
+function count() {
+  let stringarray = string.value.split('');
+  let noofwords = stringarray.length;
+
+  
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div
+      class="w-full h-screen bg-gradient-to-tr from-blue-500 via-indigo-400 to-purple-700 grid place-content-center grid-cols-1 brightness-10"
+    >
+      <form class="">
+        <h4
+          class="text-center text-gray-800 mb-5 text-3xl font-bold"
+          style="text-shadow: -3px 5px 7px rgba(0, 0, 0, 0.6)"
+        >
+          WORD COUNTER
+        </h4>
+        <fieldset
+          class="border w-10/12 p-5 mx-auto rounded-bl-2xl rounded-tr-2xl shadow-lg"
+        >
+          <legend
+            class="rounded-bl-2xl rounded-tr-2xl font-bold text-gray-700 border p-2"
+          >
+            TEXT GOES HERE
+          </legend>
+          <textarea
+            v-model="string"
+            placeholder="TEXT GOES HERE"
+            class="placeholder-gray-600 rounded-tr-2xl text-balance rounded-bl-2xl bg-transparent border-2 border-indigo-300 shadow outline-none h-72 w-full p-3 text-black"
+            autofocus
+          ></textarea>
+          <div class="flex justify-end">
+            <button
+              @click="count"
+              class="border px-5 py-3 rounded-bl-2xl rounded-tr-2xl"
+            >
+              Begin countng
+            </button>
+          </div>
+        </fieldset>
+      </form>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
